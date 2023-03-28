973debe8d6397c880a6c9e7285652699b00b5bd8
Gael <gaelzanon@hotmail.com>
2023-03-28 18:59:52 +0200

    Backend base

--- src/main/java/com/aldia/aldia/Controller/UserController.java
+++ src/main/java/com/aldia/aldia/Controller/UserController.java
@@ -1,6 +1,7 @@
 package com.aldia.aldia.Controller;
 
 
+import com.aldia.aldia.model.Usuario;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,4 +13,13 @@ public class UserController {
     public void prueba() {
         System.out.println("Ha llegado la pión");
     }
+
+    @GetMapping("/add")
+    public String registrar(@RequestParam(value = "token", defaultValue = "") String token) {
+        System.out.println("Hola");
+        GestorUsuarios gestorUsuarios = GestorUsuarios.getInstance();
+        gestorUsuarios.add(new Usuario("UserUS", "ContraDeUS", "User", "user@mail.com", "testus"));
+        return "funciona";
+    }
+
 }
